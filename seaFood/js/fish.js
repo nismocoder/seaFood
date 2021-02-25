437c7308a0917de87941d9af3949724c726a5532
nismocoder <boostedrb240sx@gmail.com>
2021-02-25 23:03:20 +0700

    organized and simplified the files and added an aside nav bar

--- seaFood/js/fish.js
+++ seaFood/js/fish.js
@@ -12,3 +12,51 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// function darkMode() {
+//   var element = document.getElementsByClassName("content");
+//   element.classList.toggle("dark-mode");
+// }
+
+function darkMode() {
+  var element = document.querySelector("content");
+  element.classList.toggle("dark-mode");
+}
+
+
+
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+     let getSidebar = document.querySelector(".nav-sidebar");
+     let getSidebarUl = document.querySelector(".nav-sidebar ul");
+     let getSidebarTitle = document.querySelector(".nav-sidebar span");
+     let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+     if (toggleNavStatus === false) {
+         getSidebarUl.style.visibility = "visible";
+         getSidebar.style.width = "272px";
+         getSidebarTitle.style.opacity = "0.4";
+
+         let arrayLength = getSidebarLinks.length;
+         for (let i = 0; i < arrayLength; i++) {
+             getSidebarLinks[i].style.opacity = "1";
+         }
+
+         toggleNavStatus = true;
+     }
+
+     else if (toggleNavStatus === true) {
+        getSidebar.style.width = "36px";
+        getSidebarTitle.style.opacity = "1";
+
+        let arrayLength = getSidebarLinks.length;
+        for (let i = 0; i < arrayLength; i++){
+            getSidebarLinks[i].style.opacity = "0";
+        }
+
+        getSidebarUl.style.visibility = "hidden";
+
+        toggleNavStatus = false;
+    }
+}
